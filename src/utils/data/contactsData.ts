ad5e016a1c1bc5e7bdca011698ec38ea6f41440e
nathanredblur <jon.nathan.rich@gmail.com>
2024-05-29 23:59:23 -0500

    update linkedin an CV URL

--- src/utils/data/contactsData.ts
+++ src/utils/data/contactsData.ts
@@ -1,11 +1,11 @@
 export const contactsData = {
-    email: 'jon.nathan.rich@gmail.com',
-    phone: '',
-    address: '',
-    github: 'https://github.com/nathanredblur',
-    facebook: '',
-    linkedIn: 'https://www.linkedin.com/in/ricojonathan/',
-    twitter: 'https://twitter.com/NathanRedBlur',
-    stackOverflow: '',
-    devUsername: "nathanredblur",
-}
+  email: "jon.nathan.rich@gmail.com",
+  phone: "",
+  address: "",
+  github: "https://github.com/nathanredblur",
+  facebook: "",
+  linkedIn: "https://www.linkedin.com/in/nathanredblur/",
+  twitter: "https://twitter.com/NathanRedBlur",
+  stackOverflow: "",
+  devUsername: "nathanredblur",
+};
