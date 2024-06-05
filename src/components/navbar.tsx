5acb533f8098aa023a9fac98b7de7bc448a13a6b
nathanredblur <jon.nathan.rich@gmail.com>
2024-06-05 11:25:29 -0500

    Contact link and smoth scroll

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -74,6 +74,16 @@ const Navbar = () => {
               </div>
             </a>
           </li>
+          <li>
+            <a
+              className="block px-4 py-2 no-underline outline-none hover:no-underline"
+              href="/#contact"
+            >
+              <div className="text-sm text-white transition-colors duration-300 hover:text-pink-600">
+                CONTACT
+              </div>
+            </a>
+          </li>
         </ul>
       </div>
     </nav>
